c9eeb7b7a3c81325fa2da7eadd55e19cebc2b0f9
rezan83 <rezan.moh83@gmail.com>
2023-06-08 05:39:29 +0200

    deployment test 20

--- api/scripts/after_install.sh
+++ api/scripts/after_install.sh
@@ -4,3 +4,6 @@ sudo chmod -R 777 /home/ubuntu/watchers-app
 cd /home/ubuntu/watchers-app
 
 sudo npm install
+
+# if pgrep ts-node; then pkill ts-node; fi
+# if pgrep node; then pkill node; fi
--- api/scripts/xxxxxxxapplicatxxxxion_stxxop.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-if pgrep ts-node; then pkill ts-node; fi
-if pgrep node; then pkill node; fi
