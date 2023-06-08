3d112f751e97a36f116b0e01051893993587ec41
rezan83 <rezan.moh83@gmail.com>
2023-06-08 04:16:28 +0200

    deployment test 18

--- api/scripts/before_install.sh
+++ api/scripts/before_install.sh
@@ -7,7 +7,7 @@
 # Install node.js and PM2 globally
 sudo apt-get update
 sudo apt-get install nodejs-legacy -y
-sudo apt-get install npm  -y
+sudo apt-get install npm -y
 sudo npm install pm2 -g
 
 # DIR="/watchers-app"
